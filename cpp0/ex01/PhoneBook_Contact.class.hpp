503bd9b553b1e2994747e7f3a38a805876724391
zaboulaza <smailnour94@gmail.com>
2025-11-08 21:36:34 +0100

    ex00 donne need to do ex01

---
+++ cpp0/ex01/PhoneBook_Contact.class.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook_Contact.class.hpp                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsmail <nsmail@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 20:43:33 by nsmail            #+#    #+#             */
+/*   Updated: 2025/11/08 21:30:37 by nsmail           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PHONEBOOK_CONTACT_CLASS_HPP
+# define PHONEBOOK_CONTACT_CLASS_HPP
+
+class PhoneBook {
+    
+    public:
+    char *str[8];
+    
+}
+
+#endif
