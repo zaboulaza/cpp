681f1455310920a5e5e02842a504b7cb9b1db586
nsmail <zaboulaza@DESKTOP-9K9IDJ3.localdomain>
2025-12-30 14:41:08 +0100

    need to do ex02

--- cpp8/ex01/Span.cpp
+++ cpp8/ex01/Span.cpp
@@ -6,9 +6,60 @@
 /*   By: zaboulaza <zaboulaza@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 18:25:05 by zaboulaza         #+#    #+#             */
-/*   Updated: 2025/12/29 18:25:16 by zaboulaza        ###   ########.fr       */
+/*   Updated: 2025/12/30 14:36:30 by zaboulaza        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
+Span::Span(const Span &span){
+    *this = span;
+}
+
+Span::Span(unsigned int N) : _N(N) {};
+
+Span &Span::operator=(const Span &span){
+    if(this != &span){
+        this->_N = span._N;
+    }
+    return (*this);
+}
+
+void Span::addNumber(int num){
+    if (tab.size() >= this->_N){
+        throw TooMuchElem();
+        return ;
+    }
+    tab.push_back(num);
+    return;
+}
+
+int Span::shortestSpan(){
+    if (this->tab.size() < 2){
+        throw NeedMoreElem();
+    }
+    int count;
+    count = abs(tab[0] - tab[1]);
+    for (int i = 0; this->tab[i]; i++){
+        for(int j = i + 1; this->tab[j]; j++){
+            if (abs(tab[i] - tab[j]) < count )
+                count = abs(tab[i] - tab[j]);
+        }
+    }
+    return (count);
+}
+
+int Span::longestSpan(){
+    if (this->tab.size() < 2){
+        throw NeedMoreElem();
+    }
+    int count;
+    count = abs(tab[0] - tab[1]);
+    for (int i = 0; this->tab[i]; i++){
+        for(int j = i + 1; this->tab[j]; j++){
+            if (abs(tab[i] - tab[j]) > count )
+                count = abs(tab[i] - tab[j]);
+        }
+    }
+    return (count);
+}
--- cpp8/ex01/Span.hpp
+++ cpp8/ex01/Span.hpp
@@ -6,26 +6,44 @@
 /*   By: zaboulaza <zaboulaza@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 18:25:20 by zaboulaza         #+#    #+#             */
-/*   Updated: 2025/12/29 18:35:17 by zaboulaza        ###   ########.fr       */
+/*   Updated: 2025/12/30 14:24:05 by zaboulaza        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
 class Span {
 
     public:
 
-        Span();
-        ~Span();
+        Span() : _N(0) {};
+        ~Span() {};
         Span(const Span &span);
         Span(unsigned int N);
         Span &operator=(const Span &span);
 
-        void addNumber();
+        void   addNumber(int num);
+        int    shortestSpan();
+        int    longestSpan();
 
+        class TooMuchElem : public std::exception {
+            const char *what() const throw(){
+                return ("Had already too much element in our vector");
+            }
+        };
+        
+        class NeedMoreElem : public std::exception {
+            const char *what() const throw(){
+                return ("We need more elem");
+            }
+        };
+    
     private:
 
+        std::vector<int> tab;
         unsigned int _N;
 
 };
